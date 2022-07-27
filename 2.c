841e42ddb7aca34a25acd8f1c99e0ea190ec0955
iamankithkumar <110085285+iamankithkumar@users.noreply.github.com>
2022-07-27 12:45:15 +0530

    Bubble Sort code done

--- 2.c
+++ 2.c
@@ -40,3 +40,19 @@ void selectionSort(int *A, int n){
         A[indexOfMin] = temp;
     }
 }
+void bubbleSort(int *A, int n){
+    int temp;
+    int isSorted = 0;
+    for (int i = 0; i < n-1; i++) // For number of pass
+    {
+        printf("Working on pass number %d\n", i+1); 
+        for (int j = 0; j <n-1-i ; j++) // For comparison in each pass
+        {
+            if(A[j]>A[j+1]){
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp; 
+            }
+        }  
+    } 
+}
