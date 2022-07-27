26816cdd832c934f88b94077266ae0602e4cf462
iamankithkumar <110085285+iamankithkumar@users.noreply.github.com>
2022-07-27 12:43:02 +0530

    Insertion Sort code done

--- 2.c
+++ 2.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
-void printArray(int* A, int n){
+void printArray(int* A, int n)
+{
     for (int i = 0; i < n; i++)
     {
         printf("%d ", A[i]);
     }
     printf("\n");
+}
+void insertionSort(int *A, int n){
+    int key, j;
+    // Loop for passes
+    for (int i = 1; i <= n-1; i++)
+    {
+        key = A[i];
+        j = i-1;
+        // Loop for each pass
+        while(j>=0 && A[j] > key){
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = key;
+    }
+}
