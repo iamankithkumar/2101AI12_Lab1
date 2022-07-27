02b69c38e15c8004b8a6bdad77d5aacd762c63b6
iamankithkumar <110085285+iamankithkumar@users.noreply.github.com>
2022-07-27 12:44:02 +0530

    Selection Sort code done

--- 2.c
+++ 2.c
@@ -22,3 +22,21 @@ void insertionSort(int *A, int n){
         A[j+1] = key;
     }
 }
+void selectionSort(int *A, int n){
+    int indexOfMin, temp;
+    printf("Running Selection sort...\n");
+    for (int i = 0; i < n-1; i++)
+    {
+        indexOfMin = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if(A[j] < A[indexOfMin]){
+                indexOfMin = j;
+            }
+        }
+        // Swap A[i] and A[indexOfMin]
+        temp = A[i];
+        A[i] = A[indexOfMin];
+        A[indexOfMin] = temp;
+    }
+}
